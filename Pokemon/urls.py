e525c511cc57229959db5bbf6425924efc6bec18
koku ulrich GBLOKPO <ulrichgblokpo@gmail.com>
2023-03-28 01:53:25 +0200

    generate dynamic secret key

--- Pokemon/urls.py
+++ Pokemon/urls.py
@@ -5,10 +5,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    path("pokemon/add",views.registerPokemon, name="register-csv-pokemon"),
     path("pokemon",views.pokemon, name="all-detail-filter"),
     path("pokemon/",views.pokemon, name="all-detail-filter"),
     path("pokemon/<str:id>",views.pokemon, name="all-detail-filter"),
-    path("type/add",views.registerPokemonType, name="all-type"),
     path("type",views.type, name="all-type"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
